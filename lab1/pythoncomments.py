33f7de949c0c7ba386a01378ec63ba237fbc0c42
David Sebelev <david.seberev@mail.ru>
2025-01-20 15:18:08 +0500

    added comm

---
+++ lab1/pythoncomments.py
@@ -0,0 +1,22 @@
+#this is a comment 
+print("hello world")
+
+#also 
+print("hello world") #here
+
+#also we can make a comment to turn off some part of code
+
+#print("hello world")
+
+
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
