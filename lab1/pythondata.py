670d9be5790e26a5f930b4788d9541a237783afa
David Sebelev <david.seberev@mail.ru>
2025-01-20 20:47:21 +0500

    added data

---
+++ lab1/pythondata.py
@@ -0,0 +1,14 @@
+#text str
+#numberic int float and complex
+#sequence list tuple and range
+#mapping type dict
+#set set frozenset
+#bool bool
+#binary bytes byteaaray and memmoryview
+#none NoneType
+
+#getting type by type()
+
+
+#diff between list and tuple is that set[] and tuple()
+#dict its dictiinary like map in c++
