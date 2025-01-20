49935f6f5415daf467c11fc0f21277d793410871
David Sebelev <david.seberev@mail.ru>
2025-01-20 16:00:18 +0500

    added pyvar folder

---
+++ lab1/pyvar/outvar.py
@@ -0,0 +1,25 @@
+x = "Python is awesome"
+print(x)
+
+
+#In the print() function, you output multiple variables, separated by a comma:
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+x = 5
+y = 10
+print(x + y)
+
+#but you cant make this kind of stuff
+x = 5
+y = "John"
+print(x + y)
